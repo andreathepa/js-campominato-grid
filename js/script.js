dad7e93a07bf4c1563804a7c3b75aefe0e0cb889
andreathepa <andreadepa32@gmail.com>
2023-05-28 23:38:24 +0200

    creating grid

--- js/script.js
+++ js/script.js
@@ -1,2 +1,17 @@
-const grid = document.getElementById('grid')
-console.log(grid)
+
+
+//Creare funzione per il singolo quadratino
+
+function createsquare(){
+    let square = document.createElement('<div>');
+
+    square.classList.add('square')
+    
+    return square;
+}
+
+
+//Creo l'elemento in HTML che ospiterà la griglia
+const grid = document.getElementById('grid');
+
+
