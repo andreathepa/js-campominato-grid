d4e49f88d24681126a4b96a74865e8a1e8c9c79e
andreathepa <andreadepa32@gmail.com>
2023-05-29 09:56:58 +0200

    bonus section added

---
+++ bonus/js/script.js
@@ -0,0 +1,31 @@
+function createsquare(){
+    let square = document.createElement('div');
+
+    square.classList.add('square')
+    
+    return square;
+}
+
+
+let butt = document.getElementById('butt')
+
+butt.addEventListener('click', function(){
+    const grid = document.getElementById('grid');
+
+for(i=0; i<=99; i++){
+    let square = createsquare();
+    square.innerText = i + 1;
+
+    square.addEventListener('click', function(){
+        this.classList.toggle('clicked');
+        console.log(this)
+    });
+
+    grid.append(square)
+}
+
+})
+
+
+
+//Creo l'elemento in HTML che ospiterà la griglia
